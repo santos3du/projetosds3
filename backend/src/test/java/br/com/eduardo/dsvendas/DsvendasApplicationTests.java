a09e0812bd831d35f5a0f4ac5fb7d655424a3df0
Eduardo Alves dos Santos <santosedu@live.com>
2021-05-03 19:13:58 -0300

    Primeiro commit - projetos criados

---
+++ backend/src/test/java/br/com/eduardo/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eduardo.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
