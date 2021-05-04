caa2e2b789c755bdeb92240e94a002d1b7776a14
Eduardo Alves dos Santos <santosedu@live.com>
2021-05-03 21:13:35 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
+import DataTable from "components/datatable";
+import Footer from "components/footer";
+import NavBar from "components/navbar";
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <NavBar />
+    <div className="container">
       <h1 className="text-primary">Olá Mundo</h1>
+      <DataTable />
     </div>
+    
+    <Footer />
+    </>
   );
 }
 
