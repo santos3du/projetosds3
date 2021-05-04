a305377833d9b36a169c37066065618dd5890816
Eduardo Alves dos Santos <santosedu@live.com>
2021-05-03 21:50:21 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,32 @@
+import React from "react";
+import BarChart from "components/barchart";
+import DonutChart from "components/donutChart";
 import DataTable from "components/datatable";
 import Footer from "components/footer";
 import NavBar from "components/navbar";
+
 
 function App() {
   return (
     <>
     <NavBar />
     <div className="container">
-      <h1 className="text-primary">Olá Mundo</h1>
+      <h1 className="text-primary py-3">Dashboard de vendas</h1>
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5>Todas as vendas</h5>
+            <DonutChart />
+          </div>
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary">Todas vendas</h2>
+        </div>
+    
       <DataTable />
     </div>
     
